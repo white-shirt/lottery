88d6464540d28f45d3d2b933dbad6a1fae17e8bf
white-shirt <775321614@qq.com>
2020-02-27 18:38:21 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     proxy: {
       '/api/h5': {
         // target: 'http://localhost:10008', // 本地后端测试地址
-        target: 'https://saas.yitum.com', // 线上后端地址
+        target: 'xxxxxxx', // 线上后端地址
         changeOrigin: true,
         secuer: false
       },
